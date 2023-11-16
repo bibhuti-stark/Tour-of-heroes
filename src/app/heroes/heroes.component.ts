b839ced1dafdbbb4aefed4f8b63202970f4ccbc6
Bibhuti-stark <bibhuti3005@gmail.com>
2023-11-16 09:48:57 +0530

    add a click event binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
   standalone: true,
   imports: [
+    FormsModule,
     NgFor,
+    NgIf,
+    UpperCasePipe
   ],
 })
 export class HeroesComponent {
+  
   heroes = HEROES;
 hero: Hero = {
   id: 1,
