a7b7015d254b6fccbd2e370ed3d1311bc4f4c614
Bibhuti-stark <bibhuti3005@gmail.com>
2023-11-16 09:54:49 +0530

    add the click event handler

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,8 +18,8 @@ import { FormsModule } from '@angular/forms';
 export class HeroesComponent {
   
   heroes = HEROES;
-hero: Hero = {
-  id: 1,
-  name: 'Windstorm'
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 };
-}
