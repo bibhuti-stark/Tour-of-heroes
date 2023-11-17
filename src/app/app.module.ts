096949dba3c1bccc33ee259a181eed3749c78861
Bibhuti-stark <bibhuti3005@gmail.com>
2023-11-17 15:29:30 +0530

    generate the hero detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        HeroDetailComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,5 +1,4 @@
 export interface Hero {
-    id : number;
+    id: number;
   name: string;
-
 }
