c096c609c46cb7ae6e30af33690e3fb8df79502d
Bibhuti-stark <bibhuti3005@gmail.com>
2023-12-08 10:07:55 +0530

    create hero service and update heroescomponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 @Component({
@@ -17,9 +17,10 @@ import { FormsModule } from '@angular/forms';
 }) 
 export class HeroesComponent {
   
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  constructor(private heroService: HeroService) {}
 };
